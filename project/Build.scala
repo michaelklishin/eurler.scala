49ba49dead8e93d943d237e9bfa78c376e6d86c9
Michael S. Klishin <michael@novemberain.com>
2012-06-15 16:47:25 +0400

    SBT, you piece of shit

---
+++ project/Build.scala
@@ -0,0 +1,37 @@
+import sbt._
+import Keys._
+
+object BuildSettings {
+  val buildOrganization = "com.novemberain"
+  val buildScalaVersion = "2.9.1"
+  val buildVersion = "0.1"
+
+  val buildSettings = Defaults.defaultSettings ++ Seq(organization := buildOrganization,
+                                                      scalaVersion := buildScalaVersion,
+                                                      version := buildVersion)
+}
+
+object Resolvers {
+  val typesafeRepo = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+  val sonatypeRepo = "Sonatype Releases"   at "http://oss.sonatype.org/content/repositories/releases"
+
+  val scalaResolvers = Seq(typesafeRepo, sonatypeRepo)
+}
+
+object Dependencies {
+  val specs2Version = "1.8.2"
+
+  val specs2 = "org.specs2" %% "specs2" % specs2Version
+}
+
+object EulerBuild extends Build {
+
+  import Resolvers._
+  import Dependencies._
+  import BuildSettings._
+
+  val commonDeps = Seq(specs2)
+
+  lazy val euler = Project("euler", file("."), settings = buildSettings ++ Seq(resolvers := scalaResolvers,
+                                                                               libraryDependencies := commonDeps))
+}
