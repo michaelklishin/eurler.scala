404a816f5cfd52803ead0e2ce8da4d4d7034269a
Michael S. Klishin <michael@novemberain.com>
2012-06-16 18:56:05 +0400

    Working around more SBT bullshit

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildSettings {
   val buildOrganization = "com.novemberain"
-  val buildScalaVersion = "2.9.1"
+  val buildScalaVersion = "2.9.2"
   val buildVersion = "0.1"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(organization := buildOrganization,
